cfdb252cf570906619abc599cb2bacbe2ae2b37b
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-04-18 23:50:27 +0530

    day_35 with python

---
+++ Day_35/maxCons1s.py
@@ -0,0 +1,30 @@
+# // Q. Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+# // 485. LeetCode - Easy
+
+# // Example 1:
+
+# // Input: nums = [1,1,0,1,1,1]
+# // Output: 3
+# // Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+# // Example 2:
+
+# // Input: nums = [1,0,1,1,0,1]
+# // Output: 2
+
+
+def max_cons_1(nums):
+    max_one = 0
+    count = 0
+    i = 0
+
+    while i < len(nums):
+        if nums[i] == 1:
+            count += 1
+            max_one = max(count, max_one)
+        elif nums[i] != 1:
+            count = 0
+
+        i += 1    
+
+    return max_one                    
---
+++ Day_35/singleNumber.py
@@ -0,0 +1,41 @@
+# // Q. Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+# // You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+# // 136. LeetCode - Easy
+
+# // Example 1:
+# // Input: nums = [2,2,1]
+# // Output: 1
+
+#  Example 2:
+#  Input: nums = [4,1,2,1,2]
+#  Output: 4
+
+
+### --------------- Solution ---------------
+
+# The above question can be done using multiple ways like xor, sorting and using dictionary 
+
+# The equivalent of Map() data structure in python is dictionary. How dictionary works, see the below code
+
+def single_num(nums):
+    # creating an empty dictionary
+    freq = {}
+
+    for num in nums:
+        freq[num] = freq.get(num, 0) + 1
+
+    for key, value in freq.items():
+        if value == 1:
+            return key        
+
+
+
+# freq[num] = freq.get(num, 0) + 1
+
+# dict.get(key, default):
+# This method is used to safely get the value for a key in a dictionary.
+
+# If the key exists, it returns the value.
+
+# If the key doesn’t exist, it returns the default value you provide.
